48b49100e05b233618e47da5b5fc11cf0d3b71de
Tom Deneau <tom.deneau@gmail.com>
2023-02-20 18:52:51 -0600

    checkpoint with minor validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-reactive-forms-example';
+export class AppComponent implements OnInit {
+    myForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.myForm = this.fb.group({
+            name: ['Sammy', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            message: ['', [Validators.required, Validators.minLength(15)]],
+        });
+        // this.myForm = new FormGroup({
+        //     name: new FormControl('Sammy'),
+        //     email: new FormControl(''),
+        //     message: new FormControl('')
+        // });
+    }
+    ngOnInit() {
+    }
+    
+    onSubmit(form: FormGroup) {
+        console.log('Valid?', form.valid); // true or false
+        console.log('Name', form.value.name);
+        console.log('Email', form.value.email);
+        console.log('Message', form.value.message);
+    }
 }
